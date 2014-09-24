4ab37bc87e70434be49e0423dc676c725bddebc1
Snow <kanel.nick@gmail.com>
2014-09-24 14:16:47 -0400

    Got to Guards

--- syntax-in-functions.hs
+++ syntax-in-functions.hs
@@ -1,3 +1,4 @@
+import Text.Printf
 -- ([Typeclass] [var]) says that [var] must be in a certain typeclass in order to proceed. You can also have more than one restriction. (Int a, Show a, Read a)
 lucky :: (Integral a) => a -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
@@ -17,7 +18,7 @@ sayMe 5 = "Five!"
 sayMe x = "Not between 1 and 5."
 
 
--- It can fail if you have a pattern that can't be matched. We don't have a catch-all at the end here. 
+-- It can fail if you have a pattern that can't be matched. We don't have a catch-all at the end here.
 charName :: Char -> String
 charName 'a' = "Albert"
 charName 'b' = "Broseph"
@@ -52,6 +53,11 @@ tell (x:[]) = "This list has one item: " ++ show x ++ "."
 tell (x:y:[]) = "This list has two items: " ++ show x ++ " and " ++ show y ++ "."
 tell (x:y:_) = "This list has more than two items. The first two are " ++ show x ++ " and " ++ show y ++ "."
 
+-- Patterns (@) are a neat way of getting a value, but keeping the original values
+capital :: String -> String
+capital "" = "Empty string."
+capital all@(x:_) = "The first letter of '" ++ all ++ "' is " ++ [x] ++ "."
+
 
 
 -- RECURSION --
@@ -67,3 +73,47 @@ factorial n = n * factorial (n - 1)
 length' :: (Num b) => [a] -> b
 length' [] = 0
 length' (_:x) = 1 + length' x
+
+
+-- Sum function to find the sum of all of the values in a list. The values of the lsit must be in the Num typeclass. 
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+
+
+-- GUARDS --
+-- Guards are a way of testing if something is true or false based on a condition. If one guard condition fails, then it falls through to the next one. "otherwise" is 
+-- defined as True always. 
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+	| weight / height ^ 2 <= 18.5 = "You're underweight you emo you."
+	| weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+	| weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
+	| otherwise   				  = "You're a whale, congratulations!"
+
+
+-- You can use "where" to do a calculation or evaluation once. More efficient than using
+bmiTell' :: (RealFloat a, Show a) => a -> a -> String
+bmiTell' weight height
+	| bmi <= 18.5 = "You're underweight you emo you. Your BMI is " ++ printf(show bmi)
+	| bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly! Your BMI is " ++ show bmi
+	| bmi <= 30.0 = "You're fat! Lose some weight, fatty! Your BMI is " ++ show bmi
+	| otherwise   = "You're a whale, congratulations! Your BMI is " ++ printf(printf "%.2f\n" (100 :: Float))
+	where bmi = weight / height ^ 2
+
+
+-- Maximum function to tell us the maximum of two arguments.
+max' :: (Ord a) => a -> a -> a
+max' a b
+	| a > b 	= a
+	| otherwise = b
+
+
+-- Comparison function to tell the relation of one things toanother. 
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b -- We can also define functions using backticks. It is sometimes easier to read them that way. 
+	| a > b 	= GT
+	| a < b 	= LT
+	| otherwise = EQ
+
