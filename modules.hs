1bd93207f71ad5db5109b37f03081e5f304e0381
Snow <kanel.nick@gmail.com>
2015-01-15 09:43:02 -0500

    mrmvwmrvererj

--- modules.hs
+++ modules.hs
@@ -25,3 +25,48 @@ numUniques = length . nub
 -- We can simplify the name so we don't have to type Data.List every time by using 'as' in our imports. So we can say like 'D.nub', which would
 -- be the same as 'Data.List.nub.' We do this by importing like
 -- ::: 'import qualified Data.List as D'
+
+-- DATA.LIST --
+-- Data.List is all about lists, and it's got functions like map and filter in it. We don't need to qualify the import becuase it's got no
+-- functions that clash with Prelude. 
+-- intersperse takes an element and a list and puts that element in between each element of the list
+inter :: String
+inter = intersperse '.' "MONKEY"
+
+-- intercalate takes a list and a list of lists, then puts the list in between each of the lists in the list of lists and then flattens it all
+interCalc :: [Int]
+interCalc = intercalate [0,0,0] [[1,2,3], [4,5,6], [7,8,9]]
+
+-- transpose takes a list of lists and then "turns" them into a 2D matrix and returns the columns as lists of lists
+transposeExample :: [[Int]]
+transposeExample = transpose [[1,2,3],[4,5,6],[7,8,9]]
+-- If we had the polynomials 3x^2 + 5x + 9, 10x^3 + 9 and 8x^3 + 5x^2 + x - 1, and we wanted to sum them all up, we can use transpose
+transposePoly :: [Int]
+transposePoly = map sum $ transpose [[0,3,5,9], [10,0,0,9], [8,5,1,-1]]
+
+-- concat flstens a list of lists into a list of elements
+concatTest :: [Char]
+concatTest = concat ["hello", " ","there"]
+
+-- concatMap is the same thing as mapping a function over a list and then flattening that list. 
+concatMapTest :: [Int]
+concatMapTest = concatMap (replicate 4) [1..3]
+
+-- 'and' takes a list of boolean values and returns true only if all the elements of the list are True. 
+andTest :: Bool
+andTest = and $ map (<4) [1..3]
+-- This will return False because there is an element that isn't less than three. 
+andTestF :: Bool
+andTestF = and $ map (<3) [1..3]
+
+-- 'or' is like 'and', but it returns True if ANY of the values in the list are True.
+orTest :: Bool
+orTest = or $ map (<3) [2..50]
+
+-- 'any' takes a predicate and a list and returns True if any of the values in the list satisfy the predicate.
+anyTest :: Bool
+anyTest = any (`elem` ['A'..'Z']) "HEYGUYSwhatsup"
+
+-- 'all' takes a predicate and a list and returns True only if ALL of the values in the list satisfy the predicate
+allTest :: Bool
+allTest = all (`elem` ['A'..'Z']) "HEYGUYSwhatsup"
