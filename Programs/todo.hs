ee5e4a902eb0eccfb86933894887faaafd9d79d2
Snow <kanel.nick@gmail.com>
2015-03-13 15:21:29 -0400

    Command line arguments are cool.

---
+++ Programs/todo.hs
@@ -0,0 +1,15 @@
+import System.IO
+import Data.List
+import Data.Char as C
+import System.Directory
+import System.Enviornment
+
+dispatch :: [(String, [String] -> IO ())]
+dispatch = [("add", add),
+			("remove", remove),
+			("view", view)]
+
+main = do
+	(command:args) <- getArgs
+	let (Just action) = lookup (map C.toLower command) dispatch
+	action args
